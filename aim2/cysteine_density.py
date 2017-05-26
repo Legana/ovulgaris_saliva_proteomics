3aace901c77098eedde8a3d0f80a60b9485e275f
Legana <legana.fingerhut@my.jcu.edu.au>
2017-05-26 15:52:54 +1000

    determining the presence of a cysteine knot in a sequence

--- aim2/cysteine_density.py
+++ aim2/cysteine_density.py
@@ -7,20 +7,42 @@
 import argparse
 import sys
 
+MAX_C_COUNT = 5;
+FRAME_SIZE = 10;
+FRAME_ADVANCE = 10;
+CYSTEINE = 'C'
+
 parser = argparse.ArgumentParser(description='Calculate cysteine density for protein sequences.')
 
 parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),default=sys.stdin)
 args = parser.parse_args()
-
-###### USE THIS AS TEMPLATE. CREATE A FOR LOOP WITHIN THE OTHER FOR loop
-
-
-#to read from a FASTA file with a loop over entries using SeqIO define the FASTA sequences and analyse them by ProteinAnalysis
-#display the sequence names, molecular weight and isoelectric point
 
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 from Bio import SeqIO
 for record in SeqIO.parse(args.infile, "fasta"):
     seq = str(record.seq)
     my_prot = ProteinAnalysis(seq)
-    print '{}\t {}\t {}'.format(record.id, my_prot.molecular_weight(), my_prot.isoelectric_point())
+    #print '{}\t {}\t {}\t {}'.format(record.id, my_prot.molecular_weight(), my_prot.isoelectric_point(), seq)
+
+    knotFound = False;
+    cCount = 0;
+
+#CREATE another loop for multiple, advance by 3, 5 and 10 OR copy the loop 3 times
+
+    #Looping through sequence, by size FRAME_ADVANCE
+    for i in range(0, len(seq), FRAME_ADVANCE):
+        frame = seq[i:FRAME_SIZE+i]
+
+        #print frame
+
+        #Count how many CYSTEINE are in frame
+        for j in range(len(frame)):
+            if (CYSTEINE in frame[j]):
+                cCount = cCount + 1;
+
+        #DOES A KNOT EXIST?
+        if (cCount >= MAX_C_COUNT):
+            knotFound = True;
+
+    if (knotFound):
+        print "knot found: " + record.id
